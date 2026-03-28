fec97ae7c599d0e34d36ab13187f8c9b2efd748c
Asim Riaz <asim.riaz@gmail.com>
2026-03-28 12:50:27 +0500

    Lookup and pipeline

--- Relations/REPL.ts
+++ Relations/REPL.ts
@@ -1,5 +1,6 @@
 import { db } from "./models/index.ts";
 
+
 // db.Student.find().then(students => {
 //     students.forEach(async student => {
 //         let result = await db.Mark.updateMany(
@@ -35,14 +36,56 @@ import { db } from "./models/index.ts";
 //   .populate('marks')
 //   .then((students) => console.log(JSON.stringify(students, null, 4)));
 
+// db.Student.aggregate([
+//     {
+//         $lookup: {
+//             from: "marks", // collection name in MongoDB
+//             localField: "regno", // field in Student
+//             foreignField: "regno", // field in Mark
+//             as: "obtain" // output array field
+//         }
+//     },
+//     {
+//         $unwind: "$obtain" // unwind the obtain array to get individual mark documents
+//     }, 
+//     {
+//         $group:{
+//             _id:{ regno: "$regno", name: "$name" },
+//             total: { $sum: "$obtain.marks" }
+//         }
+//     }, 
+//     {
+//         $project: {
+//             _id: 0,
+//             regno: "$_id.regno",
+//             name: "$_id.name",
+//             total: 1
+//         }
+//     } 
+// ])
+// .then((res) => console.log(JSON.stringify(res, null, 4)))
+// .then(() => process.exit(0));
+
+
+
+// db.Mark.aggregate([
+//     {
+//         $lookup: { from: "students",  localField: "regno",  foreignField: "regno",  as: "student", 
+//             pipeline: [{$project: { _id: 0, name: 1 }}]
+//         }
+//     }
+// ])
+// .then((res) => console.log(JSON.stringify(res, null, 4)))
+// .then(() => process.exit(0));
+
 db.Student.aggregate([
     {
-        $lookup: {
-            from: "marks", // collection name in MongoDB
-            localField: "regno", // field in Student
-            foreignField: "regno", // field in Mark
-            as: "obtain" // output array field
+        $lookup: { from: "marks",  localField: "regno",  foreignField: "regno",  
+        pipeline: [{$lookup: { from: "heads",  localField: "hid",  foreignField: "hid",  as: "head"} }, {$unwind: "$head"}],    
+        as: "marks", 
         }
     }
 ])
-.then((res) => console.log(JSON.stringify(res, null, 4)));
+.then((res) => console.log(JSON.stringify(res, null, 4)))
+.then(() => process.exit(0));
+
